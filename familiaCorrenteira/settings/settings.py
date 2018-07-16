8bf0930e96058e8c28a317956570435ebb70a520
Eduardo <eduschadesoares@hotmail.com>
2018-07-16 17:35:02 -0300

    Allowed hosts

--- familiaCorrenteira/settings/settings.py
+++ familiaCorrenteira/settings/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'http://178.128.155.155']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
