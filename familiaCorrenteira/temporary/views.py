1c77ab3f65b904bbc9bb5f6ebdf08a424f81c3b3
Eduardo <eduschadesoares@hotmail.com>
2018-08-02 19:11:58 -0300

    Add error pages

--- familiaCorrenteira/temporary/views.py
+++ familiaCorrenteira/temporary/views.py
@@ -19,16 +19,17 @@ def home_page(request):
 
 
 def server_error(request):
-    return render(request, 'errors/500.html')
+    return render(request, 'errors/404.html')
 
 
 def not_found(request):
+    print('asuhduashuashruasrhauisfhagdhjds')
     return render(request, 'errors/404.html')
 
 
 def permission_denied(request):
-    return render(request, 'errors/403.html')
+    return render(request, 'errors/404.html')
 
 
 def bad_request(request):
-    return render(request, 'errors/400.html')
+    return render(request, 'errors/404.html')
